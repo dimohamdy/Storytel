7fe05835f77c8d14c59e2190e4d1d05a2901dea7
dimo.hamdy <dimo.hamdy@gmail.com>
2019-04-17 04:21:47 +0200

    Initial Commit

---
+++ Storytel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Storytel
+//
+//  Created by BinaryBoy on 4/17/19.
+//  Copyright © 2019 BinaryBoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
